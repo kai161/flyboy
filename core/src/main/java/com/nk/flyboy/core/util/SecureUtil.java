a89d0b616adf000ec75a8ee975e651dc583b5155
kaifeng.zhang <kaifeng.zhang@mljr.com>
2017-02-05 11:03:28 +0800

    添加secureUtil类

---
+++ core/src/main/java/com/nk/flyboy/core/util/SecureUtil.java
@@ -0,0 +1,38 @@
+package com.nk.flyboy.core.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by kai on 2017/2/5.
+ */
+public class SecureUtil {
+
+    public static String MD5(String strToHash){
+
+        String md5=null;
+
+        try {
+            MessageDigest md= MessageDigest.getInstance("MD5");
+
+            byte[] bytes= md.digest(strToHash.getBytes());
+
+            StringBuilder stringBuilder=new StringBuilder(32);
+
+            for(int i=0;i<bytes.length;i++){
+                stringBuilder.append(Integer.toString(((bytes[i]&0xff))+0x100,16).substring(1));
+            }
+
+            md5=stringBuilder.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return md5;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
