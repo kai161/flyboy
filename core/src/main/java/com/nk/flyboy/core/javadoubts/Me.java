c00e7acac37365908fa60936c1ca9081bd395730
kaifeng.zhang <kaifeng.zhang@mljr.com>
2017-07-03 10:59:05 +0800

    添加 Java Doubts 解惑实例

--- core/src/main/java/com/nk/flyboy/core/javadoubts/Me.java
+++ core/src/main/java/com/nk/flyboy/core/javadoubts/Me.java
@@ -1,5 +1,6 @@
 package com.nk.flyboy.core.javadoubts;
 
+import java.math.BigDecimal;
 import java.util.regex.Pattern;
 
 /**
@@ -11,7 +12,7 @@ public class Me {
 
     public static void main(String[] args) {
 
-        //error  replaceAll 方法第一个参数接受的的是正则表达式，"." 被当做了正则表达式 匹配所有的字符
+     /*   //error  replaceAll 方法第一个参数接受的的是正则表达式，"." 被当做了正则表达式 匹配所有的字符
         System.out.println(Me.class.getName().replaceAll(".","/")+".class");
         System.out.println("test.test1.test2".replaceAll(".","/")+".class");
 
@@ -19,6 +20,10 @@ public class Me {
         System.out.println(Me.class.getName().replaceAll("\\.","/")+".class");
         System.out.println("test.test1.test2".replaceAll(Pattern.quote("."),"/")+"class");
 
-        System.out.println(Me.class.getName().replace(".","/")+".class");
+        System.out.println(Me.class.getName().replace(".","/")+".class");*/
+
+        BigDecimal bigDecimal=new BigDecimal("2324.123121");
+        System.out.println(bigDecimal.setScale(2,BigDecimal.ROUND_HALF_UP).toString());
+
     }
 }
